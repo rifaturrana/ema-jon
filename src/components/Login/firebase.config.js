62d89925c893a7c94aa8d422be21c990e3c5788b
rifaturrana <smrifaturrana@gmail.com>
2022-06-27 00:37:21 +0600

    auth done

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyAbdmOglxhzY2zGQh-yfLqyvL3EYrvjeA0",
-  authDomain: "ema-john-simple-2a388.firebaseapp.com",
-  projectId: "ema-john-simple-2a388",
-  storageBucket: "ema-john-simple-2a388.appspot.com",
-  messagingSenderId: "98301555868",
-  appId: "1:98301555868:web:6758654e845dd2cfb62959",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
 export default firebaseConfig;
